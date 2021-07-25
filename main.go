ba14745b5feb0ab9a616d987840f355b4cc99781
Kevin Konradi <kevin.konradi@icloud.com>
2021-07-25 23:34:46 +0200

    SUSSUS AMOGUS
    EMERGENCY MEEETING

--- main.go
+++ main.go
@@ -155,7 +155,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if strings.ToLower(m.Content) == "bruhelp" {
+	if strings.ToLower(m.Content) == "brelp" {
 		var helpstring = sounds[0].message
 
 		for i := 1; i < len(sounds); i++ {
